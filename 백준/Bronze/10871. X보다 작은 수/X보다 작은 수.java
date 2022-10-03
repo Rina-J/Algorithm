2d9bfffe3d0483f67bc90db80592cdb071383a34
Rina Jang <wao8387@gmail.com>
2022-10-03 22:32:48 +0900

    [Bronze V] Title: X보다 작은 수, Time: 200 ms, Memory: 17052 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -5,21 +5,21 @@ import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
-		
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
+
         int n = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
-        
+
         StringBuilder sb = new StringBuilder();
-        
-        st = new StringTokenizer(br.readLine(), " ");  //정수 N개가 있는 수열 A
-                
+
+        st = new StringTokenizer(br.readLine(), " ");  // 정수 N개가 있는 수열 A
+
         for (int i=1; i<=n; i++) {
             int a = Integer.parseInt(st.nextToken());
             if (a < x) 
-            	sb.append(a + " ");  //수가 x보다 작으면 StringBuilder에 추가
+            	sb.append(a + " ");  // 수가 x보다 작으면 StringBuilder에 추가
         }
         System.out.print(sb);
     }
