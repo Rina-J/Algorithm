fdd194521a6674cfdaecaf2360c388f08c368819
Rina Jang <wao8387@gmail.com>
2022-10-06 23:43:20 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -3,8 +3,8 @@ import java.io.InputStreamReader;
 import java.io.IOException;
  
 public class Main {
- 
 	public static void main(String[] args) throws IOException {
+        
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
 		int N = Integer.parseInt(br.readLine());
@@ -13,8 +13,9 @@ public class Main {
 		int copy = N;
         
 		do {
-			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			N = ( (N % 10) * 10) + ( ((N / 10) + (N % 10) ) % 10 );
 			cnt++;
+            
 		} while (copy != N); 
 		
 		System.out.println(cnt);
