71e6727235fb1975940e335a74af6965402d1bab
Rina Jang <wao8387@gmail.com>
2022-10-09 22:34:33 +0900

    Update 최소， 최대.java

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,26 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
  
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-        
-		int N = in.nextInt();
-		int[] arr = new int[N];
-        
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
-		}
-        
-		in.close();
-		Arrays.sort(arr);// Arrays.sort() : 배열에 저장된 원소값을 오름차순으로 정렬해주는 메서드
-		//ㄴ 최솟값은 배열의 첫번째, 최댓값은 배열의 마지막에 위치함(arr.length-1)
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
+		int index = 0;
+		int[] arr = new int[N];
+		while(st.hasMoreTokens()) {
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
+		}
+		
+		Arrays.sort(arr);
 		System.out.print(arr[0] + " " + arr[N - 1]);
 	}
 }
