f1c0b7b23596556c89cf075931b8ff0e83799e89
Rina Jang <wao8387@gmail.com>
2022-10-11 22:06:04 +0900

    Update 최소， 최대.java

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,28 +1,26 @@
+import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+ 
  
 public class Main {
  
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-		Integer.parseInt(br.readLine());	//첫 줄 N 은 안쓰이므로 입력만 받는다.
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		int max = -1000001;
-		int min = 1000001;
-		
-		while(st.hasMoreTokens()) {
-			int val = Integer.parseInt(st.nextToken());
-			if(val>max) {
-				max = val;
-			}
-			if(val<min) {
-				min = val;
-			}
+		int index = 0;
+		int[] arr = new int[N];
+		while( st.hasMoreTokens() ) { // hasMoreTokens() 는 StringTokenizer에 토큰이 남아있으면 true, 비어있으면 false를 반환
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
 		}
-		System.out.println(min + " " + max);
+		
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[N - 1]);
 	}
 }
