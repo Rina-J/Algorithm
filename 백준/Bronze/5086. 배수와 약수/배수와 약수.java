81a1204ff8292fd421ce800174ce9b8f23c1acf4
Rina Jang <wao8387@gmail.com>
2024-05-24 11:26:02 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -10,7 +10,6 @@ public class Main {
             마지막 줄에는 0이 2개 주어짐, 0이 나오면 break;
             A가 B의 약수    B % A == 0
             A가 B의 배수    A % B == 0
-
             8 16            factor
             32 4            multiple
             17 5            neither
@@ -22,8 +21,8 @@ public class Main {
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            int A = Integer.parseInt(st.nextToken());;
-            int B = Integer.parseInt(st.nextToken());;
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
             if (A == 0 && B == 0) {
                 break;
