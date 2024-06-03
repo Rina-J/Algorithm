7481195c717cf7ea358180e3975dcd2b9a4326e0
Rina Jang <wao8387@gmail.com>
2024-06-03 17:28:19 +0900

    [Bronze III] Title: 대지, Time: 368 ms, Memory: 30268 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /*
+           옥구슬 개수 N (1 ≤ n ≤ 100000)
+           옥구슬의 위치 좌표(두개의 정수)
+           3
+           20 24
+           40 21
+           10 12
+
+           => 360
+
+           (maxX - minX) * (maxY - minY)
+           (40-10) * (24-12) = 360
+        */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] x = new int[N];
+        int[] y = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        System.out.println( (x[N-1] - x[0]) * (y[N-1] - y[0]) );
+    }
+}
