14617af42f046105c8170366bac0ae397c4118a4
Rina Jang <wao8387@gmail.com>
2024-07-03 17:31:35 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /*
+            MenOfPassion(A[], n) {
+                i = ⌊n / 2⌋;
+                return A[i]; # 코드1
+            }
+
+            n(1 ≤ n ≤ 500,000)
+            
+            첫째줄 : 코드1의 수행 횟수
+            둘째줄 : 코드1의 수행 횟수를 다항식으로 나타냈을때, 최고차항의 차수 출력
+        */
+
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
